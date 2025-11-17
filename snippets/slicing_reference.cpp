877b45ea5b584c341259799d999c04eeedaba29c
Alexander Smart <aasmart@umich.edu>
2025-11-16 22:14:27 -0500

    comments?

--- snippets/slicing_reference.cpp
+++ snippets/slicing_reference.cpp
@@ -14,7 +14,9 @@ auto main() -> int {
     Child c2 { 4, 5, 6 };
 
     Base& b = c1;
+    // slices c2 to Base, so we only set (a, b) of c1, leaving c unchanged
     b = c2;
 
+    // prints 4 5 3 due to slicing
     std::println("{} {} {}", c1.a, c1.b, c1.c);
 }
