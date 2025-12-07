54ff8a77bdb6f831c6109d5313cc9c9d704ce935
Kevin Calopisis <kevincal@umich.edu>
2025-12-06 21:25:45 -0500

    format

--- snippets/addresses.cpp
+++ snippets/addresses.cpp
@@ -3,8 +3,8 @@
 auto main() -> int {
     int32_t x[5] = { 0, 1, 2, 3, 4 };
 
-    std::println("{}",
-                 (size_t) (&x + 1)
-                   - (size_t) (x));   // this prints 20, because it's taking the address of an array of size 20 bytes
+    std::println(
+      "{}",
+      (size_t) (&x + 1) - (size_t) x);   // this prints 20, because it's taking the address of an array of size 20 bytes
     std::println("{}", (size_t) (x + 1) - (size_t) (x));   // this prints 4, trivially
 }
